69e9f25043ed8ab97265269d37cf8b849a960e8e
jeffh90 <jeffhalley420@gmail.com>
2025-10-14 19:13:11 +0100

    Implement `RecyclerView` for device list display. create `card_device.xml` to define the layout for individual IoT device items in the list. Populate the global `devices` list in `MainApp.kt` with hardcoded sample data for testing purposes.

--- app/src/main/java/ie/setu/appdevassignment1/activities/DeviceListActivity.kt
+++ app/src/main/java/ie/setu/appdevassignment1/activities/DeviceListActivity.kt
@@ -2,16 +2,56 @@ package ie.setu.appdevassignment1.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ie.setu.appdevassignment1.R
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ie.setu.appdevassignment1.databinding.ActivityDeviceListBinding
+import ie.setu.appdevassignment1.databinding.CardDeviceBinding
 import ie.setu.appdevassignment1.main.MainApp
+import ie.setu.appdevassignment1.models.DeviceModel
 
 class DeviceListActivity : AppCompatActivity() {
 
     lateinit var app: MainApp
+    private lateinit var binding: ActivityDeviceListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device_list)
+        binding = ActivityDeviceListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = DeviceAdapter(app.devices)
+    }
+}
+
+class DeviceAdapter constructor(private var devices: List<DeviceModel>) :
+    RecyclerView.Adapter<DeviceAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardDeviceBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val device = devices[holder.adapterPosition]
+        holder.bind(device)
+    }
+
+    override fun getItemCount(): Int = devices.size
+
+    class MainHolder(private val binding : CardDeviceBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(device: DeviceModel) {
+            binding.deviceTitle.text = device.title
+            binding.deviceDescription.text = device.description
+        }
     }
 }
--- app/src/main/java/ie/setu/appdevassignment1/main/MainApp.kt
+++ app/src/main/java/ie/setu/appdevassignment1/main/MainApp.kt
@@ -12,6 +12,13 @@ class MainApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        i("Device started")
+        i("Device app started")
+
+        devices.add(DeviceModel("Temperature Sensor", "Monitors ambient temperature in real time"))
+        devices.add(DeviceModel("pH Sensor", "Measures water acidity for environmental monitoring"))
+        devices.add(DeviceModel("Ultrasonic Sensor", "Used for distance and obstacle detection"))
+        devices.add(DeviceModel("Smart Light", "IoT-controlled LED light with adjustable brightness"))
+        devices.add(DeviceModel("Motion Detector", "Triggers events based on movement detection"))
     }
+
 }
